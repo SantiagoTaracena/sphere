17e93b45b77ff7c036fae49a2dc9ef9e7d37a531
yong park <yong.park123456@gmail.com>
2024-04-06 20:01:08 -0600

    fixed the post handler

--- src/frontend/scenes/widgets/MyPostWidget.jsx
+++ src/frontend/scenes/widgets/MyPostWidget.jsx
@@ -45,7 +45,16 @@ import {
         formData.append("picture", image);
         formData.append("picturePath", image.name);
       }
-
+  
+      const response = await fetch(`http://localhost:3001/posts`, {
+        method: "POST",
+        headers: { Authorization: `Bearer ${token}` },
+        body: formData,
+      });
+      const posts = await response.json();
+      dispatch(setPosts({ posts }));
+      setImage(null);
+      setPost("");
     };
   
     return (
