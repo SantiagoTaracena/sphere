25fb7a80933d06732aabaaab177b56214edd39b2
yong park <yong.park123456@gmail.com>
2024-04-06 19:58:30 -0600

    added api to get the friends and return

--- src/frontend/scenes/widgets/FriendListWidget.jsx
+++ src/frontend/scenes/widgets/FriendListWidget.jsx
@@ -6,8 +6,22 @@ import { useDispatch, useSelector } from "react-redux";
 import { setFriends } from "state";
 
 const FriendListWidget = ({ userId }) => {
+  const dispatch = useDispatch();
   const { palette } = useTheme();
+  const token = useSelector((state) => state.token);
   const friends = useSelector((state) => state.user.friends);
+
+  const getFriends = async () => {
+    const response = await fetch(
+      `http://localhost:3001/users/${userId}/friends`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    const data = await response.json();
+    dispatch(setFriends({ friends: data }));
+  };
 
   useEffect(() => {
     getFriends();
