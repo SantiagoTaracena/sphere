a34a6372b2ce2c137bbc91b132346c3fc09673e4
SantiagoTaracena <staracenapuga@gmail.com>
2024-04-06 19:04:02 -0600

    Finished login function

--- src/backend/index.js
+++ src/backend/index.js
@@ -8,6 +8,7 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import authRoutes from './routes/auth.js'
 import { register } from './controllers/auth.js'
 
 const __filename = fileURLToPath(import.meta.url)
@@ -38,6 +39,8 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 app.post('/auth/register', upload.single('picture'), register)
+
+app.use('/auth', authRoutes)
 
 const PORT = process.env.PORT || 6001
 
