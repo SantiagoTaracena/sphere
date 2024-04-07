d451e4983816ec721b665e0e2afcfc19a9f725a0
yong park <yong.park123456@gmail.com>
2024-04-06 20:00:06 -0600

    working on the post

---
+++ src/frontend/scenes/widgets/MyPostWidget.jsx
@@ -0,0 +1,96 @@
+import {
+    EditOutlined,
+    DeleteOutlined,
+  } from "@mui/icons-material";
+  import {
+    Box,
+    Divider,
+    Typography,
+    InputBase,
+    useTheme,
+    IconButton,
+  } from "@mui/material";
+  import FlexBetween from "components/FlexBetween";
+  import Dropzone from "react-dropzone";
+  import UserImage from "components/UserImage";
+  import WidgetWrapper from "components/WidgetWrapper";
+  import { useState } from "react";
+  import { useDispatch, useSelector } from "react-redux";
+  import { setPosts } from "state";
+  
+  const MyPostWidget = ({ picturePath }) => {
+    const dispatch = useDispatch();
+    const [isImage, setIsImage] = useState(false);
+    const [image, setImage] = useState(null);
+    const [post, setPost] = useState("");
+    const { palette } = useTheme();
+    const medium = palette.neutral.medium;
+  
+    return (
+      <WidgetWrapper>
+        <FlexBetween gap="1.5rem">
+          <UserImage image={picturePath} />
+          <InputBase
+            placeholder="What's on your mind..."
+            onChange={(e) => setPost(e.target.value)}
+            value={post}
+            sx={{
+              width: "100%",
+              backgroundColor: palette.neutral.light,
+              borderRadius: "2rem",
+              padding: "1rem 2rem",
+            }}
+          />
+        </FlexBetween>
+        {isImage && (
+          <Box
+            border={`1px solid ${medium}`}
+            borderRadius="5px"
+            mt="1rem"
+            p="1rem"
+          >
+            <Dropzone
+              acceptedFiles=".jpg,.jpeg,.png"
+              multiple={false}
+              onDrop={(acceptedFiles) => setImage(acceptedFiles[0])}
+            >
+              {({ getRootProps, getInputProps }) => (
+                <FlexBetween>
+                  <Box
+                    {...getRootProps()}
+                    border={`2px dashed ${palette.primary.main}`}
+                    p="1rem"
+                    width="100%"
+                    sx={{ "&:hover": { cursor: "pointer" } }}
+                  >
+                    <input {...getInputProps()} />
+                    {!image ? (
+                      <p>Add Image Here</p>
+                    ) : (
+                      <FlexBetween>
+                        <Typography>{image.name}</Typography>
+                        <EditOutlined />
+                      </FlexBetween>
+                    )}
+                  </Box>
+                  {image && (
+                    <IconButton
+                      onClick={() => setImage(null)}
+                      sx={{ width: "15%" }}
+                    >
+                      <DeleteOutlined />
+                    </IconButton>
+                  )}
+                </FlexBetween>
+              )}
+            </Dropzone>
+          </Box>
+        )}
+  
+        <Divider sx={{ margin: "1.25rem 0" }} />
+  
+      </WidgetWrapper>
+    );
+  };
+  
+  export default MyPostWidget;
