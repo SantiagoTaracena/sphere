b50e9ad6e8fb2c6849674643f51f7d966a962c9b
yong park <yong.park123456@gmail.com>
2024-04-06 20:02:39 -0600

    started post widget

---
+++ src/frontend/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,77 @@
+  import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+  import Friend from "components/Friend";
+  import WidgetWrapper from "components/WidgetWrapper";
+  import { useState } from "react";
+  import { useDispatch, useSelector } from "react-redux";
+  import { setPost } from "state";
+  
+  const PostWidget = ({
+    postId,
+    postUserId,
+    name,
+    description,
+    location,
+    picturePath,
+    userPicturePath,
+    comments,
+  }) => {
+    const [isComments, setIsComments] = useState(false);
+    const dispatch = useDispatch();
+    const token = useSelector((state) => state.token);
+    const loggedInUserId = useSelector((state) => state.user._id);
+  
+    const { palette } = useTheme();
+    const main = palette.neutral.main;
+
+  
+    const patchLike = async () => {
+      const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userId: loggedInUserId }),
+      });
+      const updatedPost = await response.json();
+      dispatch(setPost({ post: updatedPost }));
+    };
+  
+    return (
+      <WidgetWrapper m="2rem 0">
+        <Friend
+          friendId={postUserId}
+          name={name}
+          subtitle={location}
+          userPicturePath={userPicturePath}
+        />
+        <Typography color={main} sx={{ mt: "1rem" }}>
+          {description}
+        </Typography>
+        {picturePath && (
+          <img
+            width="100%"
+            height="auto"
+            alt="post"
+            style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
+            src={`http://localhost:3001/assets/${picturePath}`}
+          />
+        )}
+        {isComments && (
+          <Box mt="0.5rem">
+            {comments.map((comment, i) => (
+              <Box key={`${name}-${i}`}>
+                <Divider />
+                <Typography sx={{ color: main, m: "0.5rem 0", pl: "1rem" }}>
+                  {comment}
+                </Typography>
+              </Box>
+            ))}
+            <Divider />
+          </Box>
+        )}
+      </WidgetWrapper>
+    );
+  };
+  
+  export default PostWidget;
