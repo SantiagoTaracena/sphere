3f235d8e6ebfaaeb28c7339c79a4e841d6f24f40
SantiagoTaracena <staracenapuga@gmail.com>
2024-04-06 19:14:14 -0600

    Finished users.js

--- src/backend/controllers/users.js
+++ src/backend/controllers/users.js
@@ -33,7 +33,6 @@ export const addRemoveFriend = async (req, res) => {
     const { id, friendId } = req.params
     const user = await User.findById(id)
     const friend = await User.findById(friendId)
-
     if (user.friends.includes(friendId)) {
       user.friends = user.friends.filter((id) => id !== friendId)
       friend.friends = friend.friends.filter((id) => id !== id)
@@ -43,7 +42,6 @@ export const addRemoveFriend = async (req, res) => {
     }
     await user.save()
     await friend.save()
-
     const friends = await Promise.all(
       user.friends.map((id) => User.findById(id))
     )
@@ -52,7 +50,6 @@ export const addRemoveFriend = async (req, res) => {
         return { _id, firstName, lastName, occupation, location, picturePath }
       }
     )
-
     res.status(200).json(formattedFriends)
   } catch (err) {
     res.status(404).json({ message: err.message })
