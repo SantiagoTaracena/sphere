575b8ae3dd740db77b32bdf708f5784c8322295e
yong park <yong.park123456@gmail.com>
2024-04-06 19:55:29 -0600

    agregacion de los posts

--- src/frontend/state/index.js
+++ src/frontend/state/index.js
@@ -17,21 +17,32 @@ export const authSlice = createSlice({
     setLogin: (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
-      },
-      setLogout: (state) => {
-        state.user = null;
-        state.token = null;
-      },
-      setFriends: (state, action) => {
-        if (state.user) {
-          state.user.friends = action.payload.friends;
-        } else {
-          console.error("user friends non-existent :(");
-        }
-      },
+    },
+    setLogout: (state) => {
+    state.user = null;
+    state.token = null;
+    },
+    setFriends: (state, action) => {
+    if (state.user) {
+        state.user.friends = action.payload.friends;
+    } else {
+        console.error("user friends non-existent :(");
+    }
+    },
+
+    setPosts: (state, action) => {
+    state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+    const updatedPosts = state.posts.map((post) => {
+        if (post._id === action.payload.post._id) return action.payload.post;
+        return post;
+    });
+    state.posts = updatedPosts;
+    },
   },
 });
 
-export const { setMode, setLogin, setLogout, setFriends } =
+export const { setMode, setLogin, setLogout, setFriends, setPost  } =
   authSlice.actions;
 export default authSlice.reducer;
